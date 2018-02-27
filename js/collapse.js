ef82f339dbe6c0d775c47b1cdda58635f7ec7d47
Marc Marquez <marc.marquez@gmail.com>
2018-02-26 19:48:42 -0800

    Removed commented out code. Renamed JS scripts to collapse and routing.js

--- js/collapse.js
+++ js/collapse.js
@@ -1,9 +1,11 @@
+/* Navbar closes on click when in collapsed mode*/
 $('.navbar-nav>li>a').on('click', function(){
 	var href = this.href;
 	if (href.includes('#')){
 		$('.navbar-collapse').collapse('hide');
 	}
 });
+/* Navbar dropdown closes on click when in collapsed mode*/
 $('.dropdown-menu>a').on('click', function(){
 	var href = this.href;
 	if (href.includes('#')){
