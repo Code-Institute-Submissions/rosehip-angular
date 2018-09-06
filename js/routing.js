65361d0e86e56bf918e983b1e5b909c63f7fc29e
Marc Marquez <marc.marquez@gmail.com>
2018-09-06 11:40:19 -0700

    Moved portfolio images to its own folder.

--- js/routing.js
+++ js/routing.js
@@ -1,7 +1,6 @@
 var mainAngular = angular.module('rhApp',['ngRoute']);
  
 mainAngular.config(function ($routeProvider, $locationProvider) {
-    //$locationProvider.html5MOde(true); //Enable href routing without hashes
     
     $routeProvider
         .when('/home',{
